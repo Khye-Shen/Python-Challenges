2189994e5422c60dc9c4f93881c84bfa05171227
Khye-Shen <48036947+Khye-Shen@users.noreply.github.com>
2020-12-07 21:50:41 +0800

    Add files via upload

---
+++ AS/CLEAR THE SCREEN.py
@@ -0,0 +1,12 @@
+
+def clearscreen():
+    for i in range(10):
+        print('\n')
+
+
+choice = raw_input("Do you want to clear the screen? yes or no ")
+
+if choice == "yes":
+
+    clearscreen()
+
